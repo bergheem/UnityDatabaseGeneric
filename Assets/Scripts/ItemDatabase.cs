f0ebfac51e95946f38e678dfb32f8f38b722906e
bergheem <bergamo.fed@gmail.com>
2017-05-09 21:29:15 +0200

    Added more variables to the database

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -14,13 +14,16 @@ public class ItemDatabase : MonoBehaviour
 		itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
 		ConstructItemDatabase();
 
-		Debug.Log(database[1].Title);
+		Debug.Log(database[1].Power);
 	}
 
 	void ConstructItemDatabase()
 	{
 		for (int i = 0; i < itemData.Count; i++)
-			database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"]));
+			database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"],
+					(int)itemData[i]["stats"]["power"], (int)itemData[i]["stats"]["defence"], (int)itemData[i]["stats"]["vitality"], itemData[i]["description"].ToString(),
+					(bool)itemData[i]["stackable"], (int)itemData[i]["rarity"], itemData[i]["slug"].ToString()
+				));
 	}
 }
 
@@ -36,10 +39,38 @@ public class Item
 
 	public int Value { get; set; }
 
-	public Item(int id, string title, int value)
+	public int Power { get; set; }
+
+	public int Defence { get; set; }
+
+	public int Vitality { get; set; }
+
+	public string Description { get; set; }
+
+	public bool Stackable { get; set; }
+
+	public int Rarity { get; set; }
+
+	public string Slug { get; set; }
+
+	public Item(int id, string title, int value, 
+	            int power, int defence, int vitality, string description, 
+	            bool stackable, int rarity, string slug)
 	{
 		this.ID = id;
 		this.Title = title;
 		this.Value = value;
+		this.Power = power;
+		this.Defence = defence;
+		this.Vitality = vitality;
+		this.Description = description;
+		this.Stackable = stackable;
+		this.Rarity = rarity;
+		this.Slug = slug;
+	}
+
+	public Item()
+	{
+		this.ID = -1;
 	}
 }
