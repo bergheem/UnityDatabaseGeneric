d90905e2227c30e5e2b59f98d4c26c2709af4504
bergheem <bergamo.fed@gmail.com>
2017-05-09 20:03:54 +0200

    UI setup

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,16 @@
+﻿mpleusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
