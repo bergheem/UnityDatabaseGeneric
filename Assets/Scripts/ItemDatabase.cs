3a46cde8c80666f0f0de8c4a04df53064a7ecaeb
bergheem <bergamo.fed@gmail.com>
2017-05-09 20:25:27 +0200

    Added json dll and database and started the item class and list

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -1,16 +1,45 @@
-﻿mpleusing System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO;
 
-public class ItemDatabase : MonoBehaviour {
+public class ItemDatabase : MonoBehaviour
+{
+	private List<Item> database = new List<Item>();
+	private JsonData itemData;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+		ConstructItemDatabase();
+
+		Debug.Log(database[1].Title);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void ConstructItemDatabase()
+	{
+		for (int i = 0; i < itemData.Count; i++)
+			database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"]));
+	}
+}
+
+public class Item
+{
+	private int id;
+	private string title;
+	private int value;
+
+	public int ID { get; set; }
+
+	public string Title { get; set; }
+
+	public int Value { get; set; }
+
+	public Item(int id, string title, int value)
+	{
+		this.ID = id;
+		this.Title = title;
+		this.Value = value;
 	}
 }
